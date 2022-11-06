04ce3908e993d85a1a5e7342501430b15d3be602
ThiagoJaime <thiagojaimebarb@gmail.com>
2022-11-06 20:10:31 -0300

    validations

--- scripts/login.js
+++ scripts/login.js
@@ -1,14 +1,67 @@
-let btn = document.getElementById('btn');
-let email = document.getElementById('email');
-let senha = document.getElementById('password');
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const senha = document.getElementById('password');
+const filtro = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
 
-btn.addEventListener('click', (e)=>{
+form.addEventListener('submit', (e) => {
+  
   e.preventDefault()
+
+  checkInputs()
+
 })
-btn.addEventListener('click', function(){
-  if(email.value === "" || senha.value === ""){
-    alert('Preencha todos os campos')
-  }else{
-    window.location.href = "index.html"
+
+function checkInputs() {
+  
+  if (email.value === "") {
+    
+    errorValidation(email, 'Preencha este campo!');
+  
+  } else if (!filtro.test(email.value)) {
+    
+    errorValidation(email, 'Email inválido')
+  
+  } else {
+
+    setSuccessFor(email)
+  
   }
-})
+  
+  if (senha.value === "") {
+
+    errorValidation(senha, 'Preencha este campo!');
+
+  } else if (senha.value.length <= 4 ) {
+    
+    errorValidation(senha, 'Mínimo de 5 caracteres!');
+  
+  } else {
+
+    setSuccessFor(senha)
+
+  }
+
+  if (senha.value.length > 4 && filtro.test(email.value)) {
+    setTimeout (() => {
+    
+      window.location.href = 'index.html'
+  
+    }, 1500);
+  }
+}
+
+function errorValidation(input, message) {
+  
+  const control = input.parentElement;
+  const small = control.querySelector('small')
+
+  small.innerText = message
+
+  control.className = 'control error'
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+
+  formControl.className = 'control success'
+}
